ae09d5b121d008f3fd0c0fd6ee55a8a5d4a94659
Akshat Srivastava <akshatsrivastava200213@gmail.com>
2023-09-02 21:10:43 +0530

    initial

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    page_title = 'Home'  # Set the default title
+    return render_template ("index.html", page_title=page_title)
+
+if __name__ == "__main__": 
+    app.run(debug=True, host="0.0.0.0") 
