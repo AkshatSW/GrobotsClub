c52346b28c077ed24be142752d5463588474ffdf
Akshat Srivastava <akshatsrivastava200213@gmail.com>
2023-09-06 11:23:46 +0530

    added static herobg

---
+++ static/js/style.js
@@ -0,0 +1,11 @@
+// console.log("Js working check");
+
+// Js for header
+// Initialization for ES Users
+import {
+    Collapse,
+    Ripple,
+    initTE,
+  } from "tw-elements";
+  
+  initTE({ Collapse, Ripple });
