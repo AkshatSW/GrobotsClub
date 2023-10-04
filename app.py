c393e41b96139fe8f4f817c4205eb50203fcd5a4
Akshat Srivastava <akshatsrivastava200213@gmail.com>
2023-10-04 11:40:15 +0530

    added empowering young minds page

--- app.py
+++ app.py
@@ -3,9 +3,15 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def Home():
     page_title = 'Grobots Club'  # Set the default title
     return render_template ("index.html", page_title=page_title)
+
+
+@app.route('/EmpoweringYoungMinds')
+def EmpoweringYoungMinds():
+    page_title = 'EmpoweringYoungMinds'
+    return render_template("empoweringyoungminds.html", page_title=page_title)
 
 if __name__ == "__main__": 
     app.run(debug=True, host="0.0.0.0") 
