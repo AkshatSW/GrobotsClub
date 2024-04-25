d77831f244a52faf028e02368e37c509c5e40d3f
Akshat Srivastava <akshatsrivastava200213@gmail.com>
2024-04-25 22:48:50 +0530

    updated server config

--- wsgi.py
+++ wsgi.py
@@ -1,31 +1,22 @@
+# using render, server slows down on reactivation so restarting the server every hour
+
 import os
 import sys
 from threading import Thread
-import requests
 import time
 from app import app
-
-def keep_server_active():
-    url = 'http://127.0.0.1:5000'  # Change to your server's URL
-    timeout = 5  # Time to wait for server response in seconds
-
-    while True:
-        try:
-            response = requests.get(url, timeout=timeout)
-            if response.status_code != 200:
-                print(f"Server responded with status code {response.status_code}. Restarting server...")
-                restart_server()
-            else:
-                print("Server is active.")
-        except requests.RequestException as e:
-            print(f"Error: {e}. Restarting server...")
-            restart_server()
-
-        time.sleep(10)  # Check every 10 seconds
 
 def restart_server():
     print("Restarting server...")
     os.system("./restart_gunicorn.sh")
+
+def keep_server_active():
+    restart_interval = 3600  # Restart server every hour (3600 seconds)
+
+    while True:
+        print(f"Waiting for {restart_interval} seconds before restarting the server...")
+        time.sleep(restart_interval)
+        restart_server()
 
 if __name__ == "__main__":
     # Start the keep_server_active thread
